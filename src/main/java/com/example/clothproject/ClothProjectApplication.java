bfbc3d194786231603a8743d48b3ead515aa87dc
Huan <huan.l@outlook.com>
2021-06-16 22:32:42 +0800

    初始化工程

---
+++ src/main/java/com/example/clothproject/ClothProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clothproject/ClothProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
